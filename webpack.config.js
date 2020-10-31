8fcff2798dbf524971f03693173f167a5ee7428e
Dalibor Markovic <60052021+dalibormarkovic@users.noreply.github.com>
2020-10-31 17:00:15 +0100

    add mode

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            scriptLoading: 'defer'
+        })
+    ],
+    mode:"product"
+}
